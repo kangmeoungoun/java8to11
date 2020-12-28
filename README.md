ad108420bc15236cb73238946e0f15adb075e7e5
kangmeoungoun <kes98202@naver.com>
2020-12-29 08:42:34 +0900

    함수형 인터페이스와 람다 표현식 소개

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-![image](https://user-images.githubusercontent.com/40969203/103216958-adff3500-495a-11eb-834a-2e5a5d7e037b.png)
+#### 자바 function spec
+https://docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
+
+나마지 bi ,operator 추측해서 사용
+![image](https://user-images.githubusercontent.com/40969203/103249165-47f1cc80-49b1-11eb-81d4-eca7c2766c25.png)
+![image](https://user-images.githubusercontent.com/40969203/103249174-517b3480-49b1-11eb-93fd-cb88eb27f625.png)
--- src/main/java/me/goldapple/java8to11/Foo.java
+++ src/main/java/me/goldapple/java8to11/Foo.java
@@ -1,24 +1,56 @@
 package me.goldapple.java8to11;
 
+import java.util.function.*;
+
 public class Foo{
     public static void main(String[] args){
-        int baseNumber =0;
-        RunSomething runSomething = (number) ->{
-            //baseNumber++; 밖에 있는 값을 변경할려면 안되고 참조만 할경우는 가능하다.
-            //baseNumber final 이라고 가정하고 사용을 해서 이밖에서도 baseNumber 변경 못한다.
-            return number + 10 + baseNumber;
-        };
+        Plus10 plus10 = new Plus10();
+        System.out.println(plus10.apply(1));;
 
-        //baseNumber++;
-        methodTest(runSomething);
-    }
-    public static void methodTest(RunSomething runSomething){
-        //같은 매개변수를 넣으면 무조건 같은 값이 나와야 그게 함수형 프로그래밍
-        //외부에 있는 값을 변경 하려는 경우 도 안됨.
+        //다이렉트로 이런식으로 바로 사용가능
+        Function<Integer,Integer> plus11 = (number)->number + 10;
+        System.out.println(plus11.apply(1));
 
-        System.out.println(runSomething.doIt(1));;
-        System.out.println(runSomething.doIt(1));;
-        System.out.println(runSomething.doIt(1));;
-        System.out.println(runSomething.doIt(1));;
+        Function<Integer,Integer> multiply2 = (i)->i*2;
+
+        //함수안의 함수 곱하기 2를 먼저하고 그값을 파라미터로 리턴.
+        Function<Integer, Integer> compose = plus11.compose(multiply2);
+        System.out.println(compose.apply(2));
+        //함수안의 함수 더하기 10을 먼저 한후 그값을 파라미터로 리턴.
+        Function<Integer, Integer> andthen = plus11.andThen(multiply2);
+        System.out.println(andthen.apply(2));
+
+        //입력값 과 리턴값이 같은 경우 하나만 입력받고 사용
+        //Function 인자 2개가 같은 타입일때
+        UnaryOperator<Integer> plus12 = (i)->i+10;
+        System.out.println(plus12.apply(100));
+
+        //입력값을 한개 더 받는다
+        BiFunction<Integer,Integer,Integer> test = (a,b)->a+b;
+        System.out.println(test.apply(1,2));
+        //BiFunction 인자 3개가 같은 타입일떄
+        BinaryOperator<Integer> test2 = (a,b)->a+b;
+        System.out.println(test2.apply(10,20));;
+
+
+
+
+        //입력값을 받아서 아무값도 리턴하지 않는다.
+        Consumer<Integer> printT = (i)-> System.out.println(i);
+        printT.accept(10);
+
+        //입력값은 받지 않고 리턴값만 받는다.
+        Supplier<Integer> get10 = ()->10;
+        System.out.println(get10.get());
+
+        Predicate<String> startsWithGold = (s)->s.startsWith("gold");
+        System.out.println(startsWithGold.test("gold"));
+        Predicate<Integer> isEven = (i)->  i%2==0;
+        System.out.println(isEven.test(10));;
+
+
+
+
     }
+
 }
