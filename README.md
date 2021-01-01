02b33bb1496bbf71d9a99a9434a422c77a4b3865
kangmeoungoun <kes98202@naver.com>
2021-01-01 12:35:07 +0900

    자바8 API의 기본 메소드와 스태텍 메소드

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-### 인터페이스에 있는 모든 메소드들은 public 이다.
-### Foo ,Bar 안에 같은 이름의 default 메소드가 있고 그 두개를 동시에 구현하는 구현체가 있으면 
-### 컴파일 에러가 난다. 그럴시에는 다시 구현체에서 재정의 를 해주면 된다.
-
-![image](https://user-images.githubusercontent.com/40969203/103409757-94086100-4bab-11eb-8609-eb5f27dd4a4f.png)
+![image](https://user-images.githubusercontent.com/40969203/103433050-b80c8680-4c2d-11eb-8511-f7766ab263a2.png)
--- src/main/java/me/goldapple/java8to11/App.java
+++ src/main/java/me/goldapple/java8to11/App.java
@@ -1,13 +1,30 @@
 package me.goldapple.java8to11;
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
 
 public class App {
     public static void main(String[] args){
-      Foo foo = new DefaultFoo("goldapple");
-        foo.printName();
-        foo.printNmameUpperCase();
-        Foo.printAnything();
-        System.out.println(foo.toString());
+        List<String> name = new ArrayList<>();
+        name.add("kmo");
+        name.add("goldapple");
+        name.add("toby");
+        name.add("foo");
+        //name.forEach(System.out :: println);
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit();
+        while(spliterator.tryAdvance(System.out :: println));
+        System.out.println("=====================");
+        while(spliterator1.tryAdvance(System.out :: println));
+        System.out.println("=====================");
+        //name.removeIf(s->s.startsWith("k"));
+        name.forEach(System.out :: println);
+        System.out.println("=====================");
+        Comparator<String> compareToIgnoreCase = String :: compareToIgnoreCase;
+        name.sort(compareToIgnoreCase.reversed().thenComparing(String::compareToIgnoreCase)); //이정렬 하고 다른것또 정렬하고 싶을떄
+
     }
 }
