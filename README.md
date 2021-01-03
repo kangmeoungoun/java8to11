819bacf457df366908d77fbc53ea7d933740d3bf
kangmeoungoun <kes98202@naver.com>
2021-01-03 13:59:19 +0900

    Optional API

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-![image](https://user-images.githubusercontent.com/40969203/103435008-44c63d00-4c4c-11eb-9a48-7b7c97f16522.png)
+![image](https://user-images.githubusercontent.com/40969203/103472044-cabcc200-4dcb-11eb-85e4-dea04f46a398.png)
--- src/main/java/me/goldapple/java8to11/App.java
+++ src/main/java/me/goldapple/java8to11/App.java
@@ -1,51 +1,28 @@
 package me.goldapple.java8to11;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class App {
-    public static void main(String[] args){
-        List<OnlineClass> springClasses = new ArrayList<>();
-        springClasses.add(new OnlineClass(1,"spring boot",true));
-        springClasses.add(new OnlineClass(5,"rest api development",false));
+    public static void main(String[] args) throws InterruptedException{
+        Date date = new Date();
+        long time = date.getTime();
+        System.out.println(date);
+        System.out.println(time);
 
-        Optional<OnlineClass> optional = springClasses.stream()
-                .filter(oc -> oc.getTitle().startsWith("spring"))
-                .findFirst();
-        optional.ifPresent(oc->{
-            System.out.println(oc.getTitle());
-        });
+        Thread.sleep(1000*3);
+        Date after3Seconds = new Date();
+        System.out.println(after3Seconds);
+        after3Seconds.setTime(time);
+        System.out.println(after3Seconds);
 
-        //있을때도 만들고 없을때도 만든다. orElse createNewClasses()이코드는 무조건 실행된다.
-        //OnlineClass onlineClass = optional.orElse(createNewClasses());
-        //있을때는 안만들고 없을때는 만든다.
-        /*orElse 이미 만들어진 상수 들을 가지고 올때 사용하면 유용하고  orElseGet 동적인걸 만들떄 사용*/
-        //OnlineClass onlineClass = optional.orElseGet(App :: createNewClasses);
-        //대체할 대안이 없을떄
-        //OnlineClass onlineClass = optional.orElseThrow(IllegalStateException::new);
+        Calendar calendar = new GregorianCalendar();
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
 
-        //다시 필터 사용 해서 옵셔널 리던 받환
-        //Optional<OnlineClass> onlineClass = optional.filter(OnlineClass::isClosed);
-
-        //Optional<Optional<Progress>> progress = optional.map(OnlineClass :: getProgress);
-        //Optional<Progress> progress1 = progress.orElseThrow();
-        //progress1.orElseThrow();
-        Optional<Progress> progress1 = optional.flatMap(OnlineClass :: getProgress);
-        // ||
-        // ||
-        Optional<Optional<Progress>> progress2 = optional.map(OnlineClass :: getProgress);
-        progress2.orElse(Optional.empty());
-
-        //
-
-
-
+        Calendar kmoBirthDay = new GregorianCalendar(1991,Calendar.DECEMBER,2);
     }
 
-    private static OnlineClass createNewClasses(){
-        System.out.println("creating new online class");
-        return new OnlineClass(10 , "New Class" , false);
-    }
 }
