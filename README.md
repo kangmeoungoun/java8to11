82cc3017b6038cf88bb916d3e0e9d2cd59296738
kangmeoungoun <kes98202@naver.com>
2021-01-03 15:38:15 +0900

    Date 와 Time API

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-![image](https://user-images.githubusercontent.com/40969203/103472044-cabcc200-4dcb-11eb-85e4-dea04f46a398.png)
+##### 그리니치 평균시(Greenwich Mean Time, GMT)는 런던을 기점, 웰링턴을 종점으로 하는 협정 세계시의 빠른시간입니다.
+##### 협정 세계시(協定世界時, 프랑스어: Temps Universel Coordonné, 영어: Coordinated Universal Time, UTC)는 1972년 1월 1일부터 시행된 국제 표준시입니다. UTC는 국제원자시와 윤초 보정을 기반으로 표준화되었습니다.
+##### UTC는 그리니치 평균시(GMT)로 불리기도 하는데, UTC와 GMT는 초의 소숫점 단위에서만 차이가 나기 때문에 일상에서는 혼용되어 사용되며 기술적인 표기에서는 UTC가 사용됩니다.
+
+![image](https://user-images.githubusercontent.com/40969203/103473226-a23bc480-4dd9-11eb-9080-f8c05b479d06.png)
+![image](https://user-images.githubusercontent.com/40969203/103472051-efb13500-4dcb-11eb-9fbf-126434a0fbe3.png)
+![image](https://user-images.githubusercontent.com/40969203/103472052-f475e900-4dcb-11eb-90ab-132aac6b9b64.png)
--- src/main/java/me/goldapple/java8to11/App.java
+++ src/main/java/me/goldapple/java8to11/App.java
@@ -1,28 +1,77 @@
 package me.goldapple.java8to11;
 
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class App {
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) {
+        //기게용 API 시간을 재거나 메소드 실행시간을 비교할때 사용
+        Instant instant = Instant.now();
+        System.out.println(instant); //기준시 UTC, GMT
+        ZoneId zoneId = ZoneId.systemDefault();
+        System.out.println(zoneId);
+        ZonedDateTime zonedDateTime = instant.atZone(zoneId);  //어느 존의 기준으로 현재시간을 볼것이냐.
+        System.out.println(zonedDateTime);
+        //인간용 API
+        System.out.println("============================");
+        //코드가 동작하고 있는 서버의 로컬 시간 서버가 미국에 있으면 미국시간.
+        LocalDateTime now  = LocalDateTime.now();
+        System.out.println(now);
+        LocalDateTime birthDay = LocalDateTime.of(1991 , Month.DECEMBER , 2 , 0 , 0 , 0);
+        System.out.println(birthDay);
+
+
+        //서로 변환 가능.
+        ZonedDateTime nowInKorea = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+        System.out.println(nowInKorea);
+        Instant nowInstant = Instant.now();
+        ZonedDateTime zonedDateTime1 = nowInstant.atZone(ZoneId.of("Asia/Seoul"));
+        System.out.println(zonedDateTime1);
+        System.out.println("========================");
+        LocalDate today = LocalDate.now();
+        LocalDate thisYearBirthday = LocalDate.of(2021 , Month.DECEMBER , 2);
+
+        System.out.println(today);
+        System.out.println(thisYearBirthday);
+        Period period = Period.between(today , thisYearBirthday);
+
+
+        //휴먼용 시간 비교 PERIOD 기계용 DURATION
+        Period until = today.until(thisYearBirthday);
+        System.out.println(until.get(ChronoUnit.MONTHS));
+
+        System.out.println("==========================");
+        Instant now1 = Instant.now();
+        Instant plus = now1.plus(1 , ChronoUnit.DAYS);
+        Duration between = Duration.between(now1 , plus);
+        System.out.println(between.getSeconds());
+
+        LocalDateTime now2 = LocalDateTime.now();
+        DateTimeFormatter MMddyy = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        DateTimeFormatter basicIsoDate = DateTimeFormatter.BASIC_ISO_DATE;
+        System.out.println(now2.format(MMddyy));
+        System.out.println(System.currentTimeMillis());
+        System.out.println("==========================");
+
+        String text = "12/01/1991";
+        LocalDate parse = LocalDate.parse(text , MMddyy);
+        System.out.println(parse);
+
         Date date = new Date();
-        long time = date.getTime();
-        System.out.println(date);
-        System.out.println(time);
+        Instant instant1 = date.toInstant();
+        Date newDate1 = Date.from(instant1);
 
-        Thread.sleep(1000*3);
-        Date after3Seconds = new Date();
-        System.out.println(after3Seconds);
-        after3Seconds.setTime(time);
-        System.out.println(after3Seconds);
+        ZoneId zoneId1 = TimeZone.getTimeZone("Asia/Seoul").toZoneId();
+        System.out.println(zoneId1);
 
-        Calendar calendar = new GregorianCalendar();
-        SimpleDateFormat dateFormat = new SimpleDateFormat();
 
-        Calendar kmoBirthDay = new GregorianCalendar(1991,Calendar.DECEMBER,2);
+
+        System.out.println(now2.plus(1,ChronoUnit.WEEKS)
+        );
     }
 
 }
